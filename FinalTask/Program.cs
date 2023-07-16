da37f67979e5b55979ac513c1d2f41301d349e42
Vyacheslav Nosov <slavan133@yandex.ru>
2023-07-17 00:02:08 +0300

    all abjectives are done

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string[] array1 = new string[9] {"Мороз", "и", "солнце", ",", "день", "чудесный", "еще", "ты", "..."};
+string[] array2 = new string[array1.Length];
+Array2fromArray1(array1, array2);
+Console.WriteLine("Массив 1: ");
+PrintArray(array1);
+Console.WriteLine("Массив 2 (из элементов массива 1, которые <=3): ");
+PrintArray(array2);
+
+void Array2fromArray1(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
